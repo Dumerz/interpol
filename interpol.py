b481a929c905102ff3bf628b94860af4cc0a44d7
zremud <dumdumpaulorlan06@gmail.com>
2017-11-02 23:36:02 +0800

    started the core logic of the interpreter

--- interpol.py
+++ interpol.py
@@ -11,8 +11,9 @@ class Interpol:
 	sectionList = ""
 
 	# [var] (list) keywords of interpol
-	keywords = ['CREATE', 'RUPTURE', 'DINT', 'DSTR', 'WITH', 'GIVEME?', 'GIVEYOU!', 'GIVEYOU!!', 'STORE', 'PLUS', 'MINUS', 'TIMES', 'DIVBY', 'MODU', 'RAISE', 'ROOT', 'MEAN', 'DIST']
-
+	keywords = ['CREATE', 'RUPTURE', 'DINT', 'DSTR', 'WITH', 'GIVEME?', 'GIVEYOU!', 'GIVEYOU!!', 'STORE', 'IN', 'PLUS', 'MINUS', 'TIMES', 'DIVBY', 'MODU', 'RAISE', 'ROOT', 'MEAN', 'DIST']
+	prim_keywords = ['DINT', 'DSTR', 'GIVEME?', 'GIVEYOU!', 'GIVEYOU!!', 'STORE', 'PLUS', 'MINUS', 'TIMES', 'DIVBY', 'MODU', 'RAISE', 'ROOT', 'MEAN', 'DIST']
+	
 	# [func] initialize the [class] Interpol, trigerred if new instance [class] Interpol is created
 	def __init__(self):
 		self.print_header()
@@ -28,26 +29,53 @@ class Interpol:
 		while True:
 			self.expression = ""
 			self.sectionList = ""
-			file_name = raw_input('Enter the filename of the code to be executed: ')
-			file = open(file_name)
-			content = file.read()
-			self.expression = content.strip()	# [str func] Multiple whitespaces and tabs are removed
-			self.sectionList = self.expression.split()
-			if not self.is_valid_start(self.sectionList):
-				self.handle_error('Invalid start of section at ' + self.sectionList[0])
-			if not self.is_valid_end(self.sectionList):
-				self.handle_error('Invalid end of section at ' + self.sectionList[len(self.sectionList)-1])
+			file_name = raw_input('Enter the filename of the code to be executed: ') # get file_name of code to be executed
+			if file_name.endswith('.ipol'): # test if file_name has .ipol extension
+				try: # try if the requested file_name exists
+					file = open(file_name)
+					content = file.read()
+					self.expression = content.strip()	# [str func] Multiple whitespaces and tabs are removed
+					self.sectionList = self.expression.split()
+					self.interpret_code()	# refer to [func] interpret_code
+				except IOError:
+					self.handle_error('No such file: ' + file_name + ' was found');
+			else:
+				self.handle_error('Invalid file type: ' + file_name + ' should be *.ipol')
+
+	# [func] this is the main logic of the Interpol interpreter
+	def interpret_code(self):
+		if  self.sectionList == []: # test if section is null
+			self.handle_error('Invalid start of section')
+		elif not self.is_valid_start(self.sectionList, 0): # refer to [func] is_valid_start
+			self.handle_error('Invalid start of section at ' + self.sectionList[0])
+		elif not self.is_valid_end(self.sectionList, len(self.sectionList)-1): # refer to [func] is_valid_end
+			self.handle_error('Invalid end of section at ' + self.sectionList[len(self.sectionList)-1])
+		else:	# Traverse all the codes in the statement
+			current_code = 1
+			while True:
+				if current_code >= len(self.sectionList)-1: break # Test if the [var] current_code is the last to test; if true break
+				else:
+					if self.is_valid_start(self.sectionList, current_code): # refer to [func] is_valid_start
+						self.handle_error(self.sectionList[current_code] + ' should only be on the start of the section')
+						break
+					elif self.is_valid_end(self.sectionList, current_code):# refer to [func] is_valid_end
+						self.handle_error(self.sectionList[current_code] + ' should only be on the end of the section')
+						break
+					elif current_code == 1:
+						pass # if self.sectionList[current_code] in self.prim_keywords 
+					print(self.sectionList[current_code])
+					current_code = current_code + 1
 
 	# [func] (return Boolean) test if the start of section is valid
-	def is_valid_start(self, section):	# [param] (section) section to be tested 
-		if section[0] == self.keywords[0]:
+	def is_valid_start(self, section, pos):	# [param] (section) section to be tested 
+		if section[pos] == self.keywords[0]:
 			return True
 		else:
 			return False
 
 	# [func] (return Boolean) test if the end of section is valid
-	def is_valid_end(self, section):	# [param] (section) section to be tested 
-		if section[len(section)-1] == self.keywords[1]:
+	def is_valid_end(self, section, pos):	# [param] (section) section to be tested 
+		if section[pos] == self.keywords[1]:
 			return True
 		else:
 			return False
